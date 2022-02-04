4a6b505f5edc75ac596e686b58c7080faa62e3c1
Bruno Cabral <cabral.brc@gmail.com>
2022-02-03 19:52:16 -0800

    track image added to screen

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
+bg_track = pygame.image.load('Track.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    screen.blit(bg_track, (0,0))
     pygame.display.update()
     clock.tick(120)
