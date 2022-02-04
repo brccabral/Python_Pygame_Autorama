e3a1bbe0c8c17e755957bd681c16808ebf9b03b6
Bruno Cabral <cabral.brc@gmail.com>
2022-02-03 20:02:05 -0800

    car can rotate

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ class Car(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = (640, 360))
         self.angle = 0
         self.rotation_speed = 1.8
-        self.direction = None
+        self.direction = 0
+
+    def set_rotation(self):
+        if self.direction == 1:
+            self.angle -= self.rotation_speed
+        if self.direction == -1:
+            self.angle += self.rotation_speed
+        
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.25)
+        self.rect = self.image.get_rect(center = self.rect.center)
+    
+    def update(self):
+        self.set_rotation()
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -25,8 +37,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                car.sprite.direction += 1
+            if event.key == pygame.K_LEFT:
+                car.sprite.direction -= 1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                car.sprite.direction -= 1
+            if event.key == pygame.K_LEFT:
+                car.sprite.direction += 1
 
     screen.blit(bg_track, (0,0))
+    car.update()
     car.draw(screen)
     pygame.display.update()
     clock.tick(120)
