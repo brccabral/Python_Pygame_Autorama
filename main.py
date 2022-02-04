9cdf0b4df643cec59fd72726eb9df6e534c69b6a
Bruno Cabral <cabral.brc@gmail.com>
2022-02-03 19:51:13 -0800

    Car class init

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pygame, sys
 
+class Car(pygame.sprite.Sprite):
+    def __init__(self, w, h):
+        super().__init__()
+        # duplicate the image to avoid losing resolution
+        # when rotating it (pygame issues)
+        self.original_image = pygame.image.load('Audi.png')
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center = (640, 360))
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
