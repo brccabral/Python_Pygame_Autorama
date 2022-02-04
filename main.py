9e4db673062b169c83e068745b7b3016b0dfbb04
Bruno Cabral <cabral.brc@gmail.com>
2022-02-03 20:32:31 -0800

    format document

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame, sys
+import pygame
+import sys
+
 
 class Car(pygame.sprite.Sprite):
     def __init__(self):
@@ -7,12 +9,12 @@ class Car(pygame.sprite.Sprite):
         # when rotating it (pygame issues)
         self.original_image = pygame.image.load('Audi.png')
         self.image = self.original_image
-        self.rect = self.image.get_rect(center = (640, 360))
+        self.rect = self.image.get_rect(center=(640, 360))
         self.angle = 0
         self.rotation_speed = 1.8
         self.direction = 0
         # forward is the direction
-        self.forward = pygame.math.Vector2(0, -1) # default is upwards
+        self.forward = pygame.math.Vector2(0, -1)  # default is upwards
         self.active = False
 
     def set_rotation(self):
@@ -20,10 +22,11 @@ class Car(pygame.sprite.Sprite):
             self.angle -= self.rotation_speed
         if self.direction == -1:
             self.angle += self.rotation_speed
-        
-        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.25)
-        self.rect = self.image.get_rect(center = self.rect.center)
-    
+
+        self.image = pygame.transform.rotozoom(
+            self.original_image, self.angle, 0.25)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
     def get_rotation(self):
         # rotate_ip = rotate in place
         if self.direction == 1:
@@ -40,6 +43,7 @@ class Car(pygame.sprite.Sprite):
         self.get_rotation()
         self.accelerate()
 
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -53,7 +57,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 car.sprite.direction += 1
@@ -70,7 +74,7 @@ while True:
             if event.key == pygame.K_SPACE:
                 car.sprite.active = False
 
-    screen.blit(bg_track, (0,0))
+    screen.blit(bg_track, (0, 0))
     car.update()
     car.draw(screen)
     pygame.display.update()
